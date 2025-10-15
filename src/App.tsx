b931b32af1bdf24d53d21fda54ad13afc8aeffa3
Bob Zurad <bob@zurad.net>
2025-10-15 13:49:14 -0500

    added Floating Action Button, no action

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import {
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StatusBar, StyleSheet, Text, useColorScheme, View} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {FAB, Portal} from 'react-native-paper';
 import WelcomeSection from './components/WelcomeSection';
 import LineChartDemo from './components/LineChartDemo';
 import DataPointList from './components/DataPointList';
@@ -14,6 +9,12 @@ import DataPointList from './components/DataPointList';
 const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
+  },
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 8,
+    bottom: 8,
   },
 });
 
@@ -35,6 +36,9 @@ function App(): React.JSX.Element {
         Edit <Text style={styles.highlight}>App.tsx</Text> to change this screen
         and then come back to see your edits.
       </WelcomeSection>
+      <Portal>
+        <FAB icon="plus" style={styles.fab} onPress={() => {}} />
+      </Portal>
     </View>
   );
 }
